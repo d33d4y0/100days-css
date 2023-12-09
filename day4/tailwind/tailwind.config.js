46a7a77767791572b9d227449e9054c0fccb8250
d33d4y0 <d33d4y@gmail.com>
2023-12-10 00:12:26 +0700

    add day4

---
+++ day4/tailwind/tailwind.config.js
@@ -0,0 +1,12 @@
+// to enable tailwind intellisense in VSCode
+/** @type {import("tailwindcss").Config} */
+export default {
+  content: [
+    "./index.html",
+  ],
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [],
+}
